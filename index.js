3408d57ae87cf9f77cbfb0e6e933056a54979945
kenyaescareno-hue <kenyaescareno@gmail.com>
2026-08-23 18:27:00 -0400

    Add skills list and copyright footer to index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+javascript
+const skills = ["art", "writing", "JavaScript", "GitHub", "Adobe Photoshop"];
+
+const skillsList = document.querySelector('#Skills ul');
+
+skills.forEach(function(skill) {
+    const li = document.createElement('li');
+    li.textContent = skill;
+    skillsList.appendChild(li);
+});
+
+const today = new Date();
+const thisYear = today.getFullYear();
+
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+copyright.textContent = `© ${thisYear} Kenya Escareno`;
+footer.appendChild(copyright);
